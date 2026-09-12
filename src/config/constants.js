eab5242f9164e646cb23a00e42e8c5198f24e182
Sinan ŞİRİN <snansrin@gmail.com>
2026-09-12 17:53:10 +0300

    Create constants.js

---
+++ src/config/constants.js
@@ -0,0 +1,4 @@
+"use strict";
+const $=id=>document.getElementById(id);
+const esc=s=>String(s??"").replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
+const QUOTA_MB=500;
