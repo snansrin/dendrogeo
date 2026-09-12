d245c50fc3737a721f11cd65fbe6d1ceafd2b52a
Sinan ŞİRİN <snansrin@gmail.com>
2026-09-12 19:39:34 +0300

    Update geo.js

--- src/utils/geo.js
+++ src/utils/geo.js
@@ -1,3 +1,3 @@
-"use strict";
+"use strict"; 
 function hav(a,b,c,d){const R=6371000,r=Math.PI/180,x=Math.sin((c-a)*r/2)**2+Math.cos(a*r)*Math.cos(c*r)*Math.sin((d-b)*r/2)**2;return 2*R*Math.atan2(Math.sqrt(x),Math.sqrt(1-x));}
 function brg(a,b,c,d){const r=Math.PI/180,y=Math.sin((d-b)*r)*Math.cos(c*r),x=Math.cos(a*r)*Math.sin(c*r)-Math.sin(a*r)*Math.cos(c*r)*Math.cos((d-b)*r);return(Math.atan2(y,x)/r+360)%360;}
